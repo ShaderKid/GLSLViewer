cd5d38979b77b797e276d421bb679de9fd2a4a9d
ShaderKid <shaderkid.glsl@gmail.com>
2016-07-19 22:04:26 +0900

    first Window App

---
+++ packages/shader/shader.go
@@ -0,0 +1,88 @@
+package shader
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"os"
+	"strings"
+)
+
+func LoadShader(vertexShaderPath, fragmentShaderPath string) (uint32, error) {
+	vertexShader, err := compileShader(fileToString(vertexShaderPath), gl.VERTEX_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	fragmentShader, err := compileShader(fileToString(fragmentShaderPath), gl.FRAGMENT_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	program := gl.CreateProgram()
+
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
+
+	var status int32
+	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to link program: %v", log)
+	}
+
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	return program, nil
+
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csource, free := gl.Strs(source)
+	defer free()
+	gl.ShaderSource(shader, 1, csource, nil)
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+func fileToString(path string) string {
+	code := ""
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		code += "\n" + scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	code += "\x00"
+	return code
+}

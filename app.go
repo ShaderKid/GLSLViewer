45da53c0dd23cd134e19efc4369c1d9cd93f73ac
ShaderKid <shaderkid.glsl@gmail.com>
2016-07-20 02:08:34 +0900

    viewer basic function

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
 	"runtime"
+	"syscall"
 )
 
 const defaultWindowWidth = 640
@@ -18,6 +19,69 @@ func init() {
 func dropCallback(w *glfw.Window, files []string) {
 	for index, file := range files {
 		fmt.Println(index, file)
+	}
+}
+
+type Shader struct {
+	vertex      string
+	fragment    string
+	program     uint32
+	last_update int64
+}
+
+func newShader(vertex, fragment string) *Shader {
+	sh := &Shader{vertex: vertex, fragment: fragment}
+	sh.setProgram()
+	sh.setLastUpdate()
+	return sh
+}
+
+func (self *Shader) setProgram() {
+	program, err := shader.LoadShader(self.vertex, self.fragment)
+	self.program = program
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (self *Shader) setLastUpdate() {
+	var s syscall.Stat_t
+	syscall.Stat(self.fragment, &s)
+	sec, _ := s.Mtimespec.Unix()
+	self.last_update = sec
+}
+
+func (self *Shader) getLastUpdate() int64 {
+	var s syscall.Stat_t
+	syscall.Stat(self.fragment, &s)
+	sec, _ := s.Mtimespec.Unix()
+	return sec
+}
+
+func (self *Shader) changeShader(fragment string) {
+	self.fragment = fragment
+	self.setProgram()
+	self.setLastUpdate()
+}
+
+func (self *Shader) useProgram() {
+	gl.UseProgram(self.program)
+}
+
+func (self *Shader) checkUpdate() {
+	if self.last_update != self.getLastUpdate() {
+		self.setProgram()
+		self.setLastUpdate()
+	}
+}
+
+func (self *Shader) dropCallback(w *glfw.Window, files []string) {
+	for _, file := range files {
+		if program, err := shader.LoadShader(self.vertex, file); err == nil {
+			self.fragment = file
+			self.program = program
+			self.setLastUpdate()
+		}
 	}
 }
 
@@ -37,8 +101,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	window.SetDropCallback(dropCallback)
 
 	window.MakeContextCurrent()
 
@@ -62,10 +124,8 @@ func main() {
 	gl.BindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
 	gl.BufferData(gl.ARRAY_BUFFER, len(vertexBufferData)*4, gl.Ptr(vertexBufferData), gl.STATIC_DRAW)
 
-	program, err := shader.LoadShader("./glsl/default.vs", "./glsl/default.fs")
-	if err != nil {
-		panic(err)
-	}
+	sh := newShader("./glsl/default.vs", "./glsl/default.fs")
+	window.SetDropCallback(sh.dropCallback)
 
 	gl.Enable(gl.DEPTH_TEST)
 	gl.DepthFunc(gl.LESS)
@@ -73,7 +133,8 @@ func main() {
 	gl.ClearColor(0.0, 0.0, 0.0, 1.0)
 	for !window.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-		gl.UseProgram(program)
+		sh.checkUpdate()
+		sh.useProgram()
 
 		gl.EnableVertexAttribArray(0)
 		gl.BindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
